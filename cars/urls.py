3b5b4bf8152c927885931417f4cb5fbdee81e5ce
Christian Beckham <urenacb22@gmail.com>
2022-09-22 09:26:52 -0500

    feat: added new car url path for view function and implemented new function to get car by id

--- cars/urls.py
+++ cars/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cars/', views.cars_list)
+    path('cars/', views.cars_list),
+    path('cars/<int:pk>/', views.car_detail)
 ]
